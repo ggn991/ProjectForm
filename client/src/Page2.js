d123914de733e41beb9b7c8d90eab1d2a0060253
Gagan Ganapathi T V <86801589+ggn991@users.noreply.github.com>
2024-07-09 21:56:52 +0530

    updated the age function

--- client/src/Page2.js
+++ client/src/Page2.js
@@ -52,6 +52,47 @@ const Page2 = () => {
         document.body.classList.remove('active-modal')
     }
 
+    const hello = (ageee) => {
+        let data = ageee;
+        let dob = new Date(data);
+        let day = dob.getDate();
+        let month = dob.getMonth();
+        let year = dob.getFullYear();
+
+        let now = new Date();
+        console.log(now);
+        let yearDiff = now.getFullYear() - year;
+        let monthDiff = now.getMonth() - month;
+        let dateDiff = now.getDate() - day;
+
+        if (yearDiff < 0) console.log("invalid date");
+        else if (monthDiff > 0) {
+            console.log(yearDiff);
+        } else if (monthDiff === 0 && dateDiff >= 0) {
+            console.log(yearDiff);
+        } else {
+            yearDiff = yearDiff - 1;
+            if (monthDiff <= 0)
+                if (dateDiff > 0) monthDiff = 12 + monthDiff;
+                else monthDiff = 11 - monthDiff;
+        }
+        if (dateDiff < 0) {
+            dateDiff = 30 + dateDiff;
+            monthDiff -= 1;
+        }
+
+        if (yearDiff < 0)
+            return (<p className="text2">Invalid DOB</p>)
+        else
+            return <p className="text2">Age: {yearDiff}</p>
+
+        // document.getElementById("currentAge").innerHTML =
+        //     "Your current Age is " + yearDiff + " years "
+        //     + monthDiff + " months " + dateDiff + " days";
+
+
+
+    }
 
 
     return (
@@ -80,8 +121,8 @@ const Page2 = () => {
                         {data.length > 0 && <img className="image" src={data[0].pic} />}
                         {data.length > 0 && <p className="text">{data[0].name}</p>}
                         <div className="divv1">
-
-                            {data.length > 0 && <p className="text2">Age: {2024 - parseInt((data[0].dob).substr(0, 4))}</p>}
+                            {data.length > 0 && hello(data[0].dob)}
+                            {/* {data.length > 0 && <p className="text2">Age: {2024 - parseInt((data[0].dob).substr(0, 4))}</p>} */}
                             {data.length > 0 && <p className="text2">DOB: {data[0].dob}</p>}
                         </div>
 
